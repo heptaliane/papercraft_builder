b34a574c68d44013ce165444c9c0a693b10ef024
heptaliane <heptaliane@gmail.com>
2024-06-16 17:52:30 +0900

    Add WebGLCanvas properties

--- src/view/webgl_canvas.rs
+++ src/view/webgl_canvas.rs
@@ -1,46 +1,60 @@
+use std::rc::Rc;
+
 use web_sys::HtmlCanvasElement;
-use yew::{function_component, html, use_effect_with, use_node_ref, Html};
+use yew::{function_component, html, use_effect_with, use_node_ref, Html, Properties};
 
 use super::utils::{
     draw_triangles, get_webgl_rendering_context, rotate_view_point, set_color_data, set_ibo_data,
     set_vertex_data, setup_program, setup_vertex_array,
 };
 
-const CANVAS_WIDTH: u32 = 800;
-const CANVAS_HEIGHT: u32 = 800;
+const DEFAULT_CANVAS_WIDTH: u32 = 800;
+const DEFAULT_CANVAS_HEIGHT: u32 = 800;
 
-const VERTICES: &[[f32; 3]] = &[
-    [0.0, 0.0, 0.0],
-    [0.5, 0.0, 0.0],
-    [0.5, 0.5, 0.0],
-    [0.0, 0.5, 0.0],
-];
+const DEFAULT_AXIS: [f32; 3] = [0.0, 0.0, 1.0];
+const DEFAULT_ANGLE: f32 = 0.0;
 
-const COLORS: &[[f32; 4]] = &[
-    [1.0, 0.0, 0.0, 1.0],
-    [1.0, 0.0, 0.0, 1.0],
-    [1.0, 0.0, 0.0, 1.0],
-    [1.0, 0.0, 0.0, 1.0],
-];
+#[derive(Properties, PartialEq, Clone)]
+pub struct WebGLCanvasProps {
+    pub vertices: Rc<[[f32; 3]]>,
+    pub colors: Rc<[[f32; 4]]>,
+    pub indices: Rc<[[u16; 3]]>,
 
-const INDICES: &[[u16; 3]] = &[[0, 1, 2], [0, 2, 3]];
+    #[prop_or(DEFAULT_CANVAS_HEIGHT)]
+    pub height: u32,
+    #[prop_or(DEFAULT_CANVAS_WIDTH)]
+    pub width: u32,
+
+    #[prop_or(Rc::from(DEFAULT_AXIS))]
+    pub axis: Rc<[f32; 3]>,
+    #[prop_or(DEFAULT_ANGLE)]
+    pub rotate: f32,
+}
 
 #[function_component]
-pub fn WebGLCanvas() -> Html {
+pub fn WebGLCanvas(props: &WebGLCanvasProps) -> Html {
     let canvas_ref = use_node_ref();
 
     {
         let canvas_ref = canvas_ref.clone();
+        let props = props.clone();
 
-        use_effect_with(canvas_ref, |canvas_ref| {
+        use_effect_with(canvas_ref, move |canvas_ref| {
             let canvas = canvas_ref
                 .cast::<HtmlCanvasElement>()
                 .expect("canvas_ref not attached to canvas element");
-            canvas.set_height(CANVAS_HEIGHT);
-            canvas.set_width(CANVAS_WIDTH);
+            canvas.set_height(props.height);
+            canvas.set_width(props.width);
 
-            draw_canvas(&canvas, &VERTICES, &COLORS, &INDICES, &[0.0, 0.0, 1.0], 0.0)
-                .expect("Failed to WebGL render");
+            draw_canvas(
+                &canvas,
+                &props.vertices,
+                &props.colors,
+                &props.indices,
+                &props.axis,
+                props.rotate,
+            )
+            .expect("Failed to WebGL render");
         });
     }
 
